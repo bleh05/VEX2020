d51d85f4c811ac25b2547bcaffcaa0ba5c1e0f66
bleh0.5 <qinxiaoying@gmail.com>
2019-10-17 21:56:35 -0500

    Update autonomous.cpp

--- autonomous.cpp
+++ autonomous.cpp
@@ -1,177 +1,169 @@
 #include "main.h"
 #include <stdio.h>
 #include "okapi/api.hpp"
-
+bool red = true;
 using namespace okapi;
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
- std::vector<int> a = {};
- std::vector<int> b = {};
- std::vector<int> c = {};
- std::vector<int> d = {};
-  bool red=true;
-  bool front =false;
-  bool skills=false;
- pros::Motor intake (4);
- pros::Motor intake2 (5);
- auto chassis = ChassisControllerFactory::create(
-  {1, -2}, {10, -9},
-  AbstractMotor::gearset::green,
-  {4_in, 12.5_in}
-);
-auto profileController1 = AsyncControllerFactory::motionProfile(
-  0.20,  // Maximum linear velocity of the Chassis in m/s
-  2.0,  // Maximum linear acceleration of the Chassis in m/s/s
-  10.0, // Maximum linear jerk of the Chassis in m/s/s/s
-  chassis // Chassis Controller
-);
+#define tpi 8.8188038732
+#define tpdeg 0.82
+#define LEFT_WHEELS_PORT 2
+#define LEFT_WHEELS_PORT_2 6
+#define RIGHT_WHEELS_PORT 5
+#define RIGHT_WHEELS_PORT_2 3
+#define INTAKE_PORT_L 8
+#define INTAKE_PORT_R 7
+#define OUTTAKE_PORT 20
+#define LIFT_PORT 9
+#define VISION_PORT 17
+pros::Motor aleft_wheels (LEFT_WHEELS_PORT);
+pros::Motor aleft_wheels_2 (LEFT_WHEELS_PORT_2,true);
+pros::Motor aright_wheels (RIGHT_WHEELS_PORT, true);
+pros::Motor aright_wheels_2 (RIGHT_WHEELS_PORT_2);
+pros::Motor aintake_L (INTAKE_PORT_L);
+pros::Motor aintake_R (INTAKE_PORT_R);
+pros::Motor alift (LIFT_PORT,MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor aouttake (OUTTAKE_PORT,MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 
-auto profileController2= AsyncControllerFactory::motionProfile(
-  4.0,
-  4.0,
-  4.0,
-  chassis
-);
-void autonomous() {
-  if(skills){
-    int tick = 0;{
-      while(tick<a.size()){
-        pros::Motor(1).move(a[tick]);
-        pros::Motor(10).move(b[tick]);
-        pros::Motor(2).move(a[tick]);
-        pros::Motor(9).move(b[tick]);
-        pros::Motor(4).move(-c[tick]);
-        pros::Motor(5).move(c[tick]);
-        pros::Motor(3).move(d[tick]);
-        pros::Motor(11).move(-d[tick]);
-        tick++;
-        pros::delay(10);
-      }
+void driveForward(int inc){
+  IntegratedEncoder drive = IntegratedEncoder(aleft_wheels);
+  drive.reset();
+  for(;drive.get()>=-inc*tpi;){
+    aleft_wheels.move(-80);
+    aright_wheels.move(80);
+    aleft_wheels_2.move(80);
+    aright_wheels_2.move(80);
+    pros::delay(10);
+    printf("%fd\n",drive.get());
+  }
+    aleft_wheels.move(0);
+    aright_wheels.move(0);
+    aleft_wheels_2.move(0);
+    aright_wheels_2.move(0);
+}
+void driveBackward(int inc){
+  IntegratedEncoder drive = IntegratedEncoder(aleft_wheels);
+  drive.reset();
+  for(;drive.get()<=-inc*tpi;){
+    aleft_wheels.move(80);
+    aright_wheels.move(-80);
+    aleft_wheels_2.move(-80);
+    aright_wheels_2.move(-80);
+    pros::delay(10);
+    printf("%fd\n",drive.get());
+  }
+    aleft_wheels.move(0);
+    aright_wheels.move(0);
+    aleft_wheels_2.move(0);
+    aright_wheels_2.move(0);
+}
+void turn(int deg){
+  IntegratedEncoder drive = IntegratedEncoder(aleft_wheels);
+  drive.reset();
+  for(;drive.get()>=-tpdeg*deg;){
+    aleft_wheels.move(-60);
+    aright_wheels.move(-60);
+    aleft_wheels_2.move(60);
+    aright_wheels_2.move(-60);
+    pros::delay(10);
+    printf("%ft\n",drive.get());
+  }
+    aleft_wheels.move(0);
+    aright_wheels.move(0);
+    aleft_wheels_2.move(0);
+    aright_wheels_2.move(0);
+}
+void rturn(int deg){
+  IntegratedEncoder drive = IntegratedEncoder(aleft_wheels);
+  drive.reset();
+  for(;drive.get()<=-tpdeg*deg;){
+    aleft_wheels.move(60);
+    aright_wheels.move(60);
+    aleft_wheels_2.move(-60);
+    aright_wheels_2.move(60);
+    pros::delay(10);
+    printf("%ft\n",drive.get());
+  }
+    aleft_wheels.move(0);
+    aright_wheels.move(0);
+    aleft_wheels_2.move(0);
+    aright_wheels_2.move(0);
+}
+void lift(bool dir){//false for reverse
+  IntegratedEncoder enc = IntegratedEncoder(alift);
+  enc.reset();
+  if(dir){
+    while(enc.get()<=200){
+      alift.move(127);
     }
   }
   else{
-    int c = 2*red-1;
-    if(front){
-        intake.move(126);
-        intake2.move(-126);
-        profileController2.generatePath({
-          Point{0_ft, 0_ft, 0_deg},
-          Point{2.5_ft, 0_ft, 0_deg}},
-          "intake1"
-        );
-        printf("g");
-      profileController2.setTarget("intake1");
-        profileController2.waitUntilSettled();
-      profileController2.setTarget("intake1",true);
-        profileController2.waitUntilSettled();
+    while(enc.get()>=-200){
+      alift.move(-127);
+    }
+  }
+  alift.move(0);
+}
+void aouttake_macro(bool dir){//false for reverse
+  IntegratedEncoder enc = IntegratedEncoder(aouttake);
+  enc.reset();
+  if(dir){
+    while(enc.get()<=12000){
+      aouttake.move(127);
+    }
+  }
+  else{
+    while(enc.get()>=-12000){
+      aouttake.move(-127);
+    }
+  }
+  aouttake.move(0);
+}
+void autonomous(){
 
-        profileController2.generatePath({
-          Point{0_ft, 0_ft, 0_deg},
-          Point{0.1_ft, 0_ft, 0_deg}},
-          "forward"
-        );
-      profileController2.setTarget("forward");
-        profileController2.waitUntilSettled();
-      chassis.turnAngle(c*-140);
-      profileController2.generatePath({
-        Point{0_ft, 0_ft, 0_deg},
-        Point{1.1_ft, 0_ft, 0_deg}},
-        "forward2"
-      );
-    profileController2.setTarget("forward2");
-      profileController2.waitUntilSettled();
-      intake.move(0);
-      intake2.move(0);
-      IntegratedEncoder enc = IntegratedEncoder(pros::Motor(3));//outtake
-      enc.reset();
-
-      intake.move(0);
-      intake2.move(0);
-      while(enc.get()<=5600){
-        pros::Motor(3).move(126);
-        pros::Motor(11).move(-126);
-      }
-      pros::Motor(3).move(0);
-      pros::Motor(11).move(0);
-      //backoff
-      pros::Motor(1).move(-50);
-      pros::Motor(2).move(-50);
-      pros::Motor(10).move(-50);
-      pros::Motor(9).move(-50);
+    aouttake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    aintake_R.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    aintake_L.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    aleft_wheels.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    aright_wheels.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    aleft_wheels_2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    aright_wheels_2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    lift(true);
+    lift(false);
+    lift(true);
+    lift(false);
+    aintake_L.move(127);
+    aintake_R.move(-127);
+    driveForward(50);
+    pros::delay(200);
+    if(red){
+      turn(36);
     }
     else{
-      chassis.setMaxVelocity(200);
-      intake.move(126);
-      intake2.move(-126);
-      profileController1.generatePath({
-        Point{0_ft, 0_ft, 0_deg},
-        Point{1.5_ft, 0_ft, 0_deg}},
-        "intake1"
-      );
-      printf("done");
-    profileController1.setTarget("intake1");
-      profileController1.waitUntilSettled();
-      profileController2.generatePath({
-        Point{0_ft, c*-0.90_ft, 0_deg},
-        Point{2.5_ft, 0_ft, 0_deg}},
-        "back1" // Profile name
-      );
-    profileController2.setTarget("back1",true);
-
-    profileController1.generatePath({
-      Point{0_ft, 0_ft, 0_deg},
-      Point{2.7_ft, 0_ft, 0_deg}},
-      "intake2" // Profile name
-    );
-    profileController1.setTarget("intake2");
-    profileController1.waitUntilSettled();
-    //stopintake
-    //turn 135 degrees
-    chassis.setMaxVelocity(60);
-    chassis.turnAngle(c*204);
-    pros::delay(400);
-     
-    chassis.setMaxVelocity(200);
-    profileController2.generatePath({
-      Point{0_ft, 0_ft, 0_deg},
-      Point{2.0_ft, 0_ft, 0_deg}},
-      "scorezone" // Profile name
-    );
-    profileController2.setTarget("scorezone");
-   
-    profileController2.waitUntilSettled();
-     
-    intake.move(0);
-    intake2.move(-0);
-    pros::Motor(1).move(0);
-    pros::Motor(2).move(0);
-    pros::Motor(10).move(0);
-    pros::Motor(9).move(0);
-    IntegratedEncoder enc = IntegratedEncoder(pros::Motor(3));//outtake
-    enc.reset();
-
-    intake.move(0);
-    intake2.move(0);
-    while(enc.get()<=5600){
-      pros::Motor(3).move(126);
-      pros::Motor(11).move(-126);
+      rturn(-36);
     }
-    pros::Motor(3).move(0);
-    pros::Motor(11).move(0);
-    //backoff
-    pros::Motor(1).move(-50);
-    pros::Motor(2).move(-50);
-    pros::Motor(10).move(-50);
-    pros::Motor(9).move(-50);
-  }
-}
+    driveBackward(-55);
+    pros::delay(200);
+    if(red){
+      rturn(-36);
+    }
+    else{
+      turn(36);
+    }
+    driveForward(50);
+    pros::delay(200);
+    driveBackward(-40);
+    pros::delay(200);
+    if(red){
+      rturn(-130);
+    }
+    else {
+      turn(130);
+    }
+    aintake_L.move(30);
+    aintake_R.move(-30);
+    driveForward(10);
+    pros::delay(100);
+
+    aintake_L.move(-30);
+    aintake_R.move(30);
+    aouttake_macro(false);
 }
