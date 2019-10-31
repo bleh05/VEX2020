7021314096a29b9192135637b32a4d385384810c
bleh0.5 <qinxiaoying@gmail.com>
2019-10-30 22:10:00 -0500

    Update autonomous.cpp

--- autonomous.cpp
+++ autonomous.cpp
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include "okapi/api.hpp"
-bool red = true;
-bool risky = false;
+bool blue = true;
+bool risky= true;
 using namespace okapi;
 #define tpi 8.8188038732
 #define tpdeg 0.82
@@ -15,7 +15,7 @@ using namespace okapi;
 #define OUTTAKE_PORT 20
 #define LIFT_PORT 9
 #define VISION_PORT 17
-pros::Motor aleft_wheels (LEFT_WHEELS_PORT);
+pros::Motor aleft_wheels (LEFT_WHEELS_PORT,true);
 pros::Motor aleft_wheels_2 (LEFT_WHEELS_PORT_2,true);
 pros::Motor aright_wheels (RIGHT_WHEELS_PORT, true);
 pros::Motor aright_wheels_2 (RIGHT_WHEELS_PORT_2);
@@ -23,15 +23,26 @@ pros::Motor aintake_L (INTAKE_PORT_L);
 pros::Motor aintake_R (INTAKE_PORT_R);
 pros::Motor alift (LIFT_PORT,MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor aouttake (OUTTAKE_PORT,MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
-
+auto myChassis = ChassisControllerFactory::create(
+  {-2, 6}, // Left motors
+  {-5, 3},   // Right motors
+  AbstractMotor::gearset::red, // Torque gearset
+  {4_in, 12.5_in} // 4 inch wheels, 12.5 inch wheelbase width
+);
+auto mp = AsyncControllerFactory::motionProfile(
+  1.0,  // Maximum linear velocity of the Chassis in m/s
+  2.0,  // Maximum linear acceleration of the Chassis in m/s/s
+  10.0, // Maximum linear jerk of the Chassis in m/s/s/s
+  myChassis // Chassis Controller
+);
 void driveForward(int inc){
   IntegratedEncoder drive = IntegratedEncoder(aleft_wheels);
   drive.reset();
   for(;drive.get()>=-inc*tpi;){
-    aleft_wheels.move(-80);
-    aright_wheels.move(80);
-    aleft_wheels_2.move(80);
-    aright_wheels_2.move(80);
+    aleft_wheels.move(-50);
+    aright_wheels.move(50);
+    aleft_wheels_2.move(50);
+    aright_wheels_2.move(50);
     pros::delay(10);
     printf("%fd\n",drive.get());
   }
@@ -103,23 +114,34 @@ void lift(bool dir){//false for reverse
   }
   alift.move(0);
 }
-void aouttake_macro(bool dir){//false for reverse
-  IntegratedEncoder enc = IntegratedEncoder(aouttake);
-  enc.reset();
-  if(dir){
-    while(enc.get()<=12000){
-      aouttake.move(127);
+bool aouttake_macro(bool dir,IntegratedEncoder enc){//false for reverse
+  if(!dir){
+    if(enc.get()>=-6200){
+      if(enc.get()<-4500){
+        aouttake.move(-85);
+      }
+      else{
+        aouttake.move(-127);
+      }
+      printf("%f\n",enc.get());
+      return true;
+    }
+    else{
+      return false;
     }
   }
   else{
-    while(enc.get()>=-12000){
-      aouttake.move(-127);
+    if(enc.get()<=0){
+      aouttake.move(127);
+      return true;
+    }
+    else{
+      return false;
     }
   }
-  aouttake.move(0);
 }
 void autonomous(){
-
+ 
     aouttake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     aintake_R.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     aintake_L.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -131,63 +153,39 @@ void autonomous(){
     lift(false);
     lift(true);
     lift(false);
-    aintake_L.move(127);
-    aintake_R.move(-127);
-    if(risky){
-      driveForward(50);
-      pros::delay(200);
-      if(red){
-        turn(36);
-      }
-      else{
-        rturn(-36);
-      }
-      driveBackward(-55);
-      pros::delay(200);
-      if(red){
-        rturn(-36);
-      }
-      else{
-        turn(36);
-      }
-      driveForward(50);
-      pros::delay(200);
-      driveBackward(-40);
-      pros::delay(200);
-      if(red){
-        rturn(-130);
-      }
-      else {
-        turn(130);
-      }
-      aintake_L.move(30);
-      aintake_R.move(-30);
-      driveForward(10);
-      pros::delay(100);
-
-      aintake_L.move(-30);
-      aintake_R.move(30);
-      aouttake_macro(false);
+    if(!risky){
+      driveForward(5);
+      aintake_L.move(-127);
+      aintake_R.move(127);
     }
     else{
-      driveForward(50);
+      aintake_R.move(-127);
+      aintake_L.move(127);
+      driveForward(56);
       pros::delay(200);
-      driveBackward(-52);
-      pros::delay(200);
-      driveForward(5);
-      if(red){
-        turn(90);
+      turn(25);
+      driveBackward(-60);
+      driveBackward(-12);
+      driveForward(58);
+      pros::delay(400);
+      driveBackward(-60);
+      driveForward(7);
+      rturn(-67);
+      driveForward(9);
+      aintake_L.move(-25);
+      aintake_R.move(25);
+      bool keep = aouttake_macro(false,IntegratedEncoder(aouttake));
+      while (keep){
+        keep = aouttake_macro(false,IntegratedEncoder(aouttake));
       }
-      else{
-        rturn(-90);
+      driveBackward(-5);
+      keep = aouttake_macro(true,IntegratedEncoder(aouttake));
+      while (keep){
+        keep = aouttake_macro(true,IntegratedEncoder(aouttake));
       }
-      aintake_L.move(30);
-      aintake_R.move(-30);
-      moveForward(24);
-      pros::delay(100);
-      aintake_L.move(-30);
-      aintake_R.move(30);
-      aouttake_macro(false);
-      moveBackward(10);
+      aouttake.move(0);
+      //mp.generatePath({Point{3.5_ft, 2_ft, 0_deg}, Point{0_ft, 0_ft, 0_deg}}, "spline");
+      //mp.setTarget("spline");
+      //mp.waitUntilSettled();
     }
 }
